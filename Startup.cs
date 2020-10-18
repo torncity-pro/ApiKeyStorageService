3d4f30ae48590b4c13bbe2ca6c92631e6a25427d
tornapi <admin@torncity.pro>
2020-10-18 14:50:55 -0700

    Update Startup.cs
    Change connection string name

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace ApiKeyStorageService
             services.AddControllers();
 
             services.AddDbContext<TornApiKeyContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("TornApiKeyContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("TornApiKeyDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
